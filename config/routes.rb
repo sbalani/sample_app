28591dbbf9eedf844d72309f2a6c0cb19dfc6e1c
sbalani <sharan.balani@ripple-wave.com>
2012-12-06 13:54:21 +0800

    Finish layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 SampleApp::Application.routes.draw do
+root to: 'static_pages#home'
 
-root to: 'welcome#index'
-  match '/help', to: 'static_pages#help'
-    match '/about', to: 'static_pages#about'
-      match '/contact', to: 'static_pages#contact'
+  match '/signup',  to: 'users#new'
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
 
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,61 +2,51 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
 	describe "Home page" do
 
-		it "should hav the content 'Sample App'" do
-			visit root_path
-			page.should have_selector('h1', text: 'Sample App') 
-    end
+    before { visit root_path }
 
-  	it "should have the base title" do
-  			visit root_path
+      it {should have_selector('h1', text: 'Sample App') }
+      it { should have_selector 'title',
+                    text: full_title('')) }
+      it {should_not have_selector('title', text: '| Home') }
 
-  			page.should have_selector('title',
-                    text: "Ruby on Rails Tutorial Sample App | Home")
-    end
+    describe "Help page" do
 
-    it "should not have a custom page title" do
-    visit root_path
-    page.should_not have_selector('title', text: '| Home')
-  	end
+    before {visit help_path}
+
+		  it {should have_selector('h1', text: 'Help')  }
+  	 it {should have_selector('title',
+                    text: full_title('Help'))  }
+		
   end
 
-  describe "Help page" do
-		it "should hav the content 'Help'" do
-			visit help_path
-			page.should have_selector('h1', text: 'Help')  
-  		end
-  		it "should have the right title" do
-  			visit help_path
-  			page.should have_selector('title',
-                    text: "Ruby on Rails Tutorial Sample App | Help")
-		end
-
-  	end
-
 	describe "About page" do
-		it "should hav the content 'About Us'" do
-			visit about_path
-			page.should have_selector('h1', text: 'About Us')  
-  		end
-		it "should have the right title" do
-  			visit about_path
-  			page.should have_selector('title',
-                    text: "Ruby on Rails Tutorial Sample App | About Us")
-		end
 
-  	end
+    before { visit about_path}
+		it {should have_selector('h1', text: 'About Us')  }
+		it {should have_selector('title',
+                    text: full_title('About'))  }
+		
+  end
+  
   	describe "contact page" do
-		it "should hav the content 'Contact'" do
-			visit contact_path
-			page.should have_selector('h1', text: 'contact')  
-  		end
-		it "should have the right title" do
-  			visit contact_path
-  			page.should have_selector('title',
-                    text: "Ruby on Rails Tutorial Sample App | contact")
-		end
 
-  	end
+      before { visit contact_path}
+		it {should have_selector('h1', text: 'contact')  }
+		it {should have_selector('title',
+                    text: full_title('contact'))  }
+  end
+
+it "should have the right links on the layout" do
+  visit root_path
+  click_link "About"
+  page.should have_selector 'title', text: full_title('About Us')
+  click_link "Help"
+  page.should have_selector 'title', text: full_title('Help')
+
+
+
 end
